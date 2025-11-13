a5f1acf01dd70556aa134edf9b7dc5ebf71f69cf
DESKTOP-D1H5I61\Belsel <cladius95@hotmail.com>
2025-11-13 11:38:24 +0100

    Initial commit (API REST)

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,7 @@
 <?php
 
-use App\Http\Controllers\GraphsController;
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
-    Route::put('/products/{product}', [ProductController::class,'update'])->name('products.update');
-    Route::get('/graphs', [GraphsController::class, 'index'])->name('graphs.index');
+    return view('welcome');
 });
-
-require __DIR__ . '/settings.php';
-require __DIR__ . '/auth.php';
